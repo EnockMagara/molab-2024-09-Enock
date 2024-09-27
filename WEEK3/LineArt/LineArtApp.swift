7f7b3f97aab41b0fd01667e131ec71ca45c3378a
Enock Mecheo <emm10042@nyu.edu>
2024-09-27 01:22:05 -0400

    updated WEEK3

---
+++ WEEK3/LineArt/LineArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineArtApp.swift
+//  LineArt
+//
+//  Created by Enock Mecheo on 26/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct LineArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
