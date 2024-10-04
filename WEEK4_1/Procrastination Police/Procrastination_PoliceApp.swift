fa1c6ac2a15229bde55e64fc153c9971034d9f3d
Enock Mecheo <emm10042@nyu.edu>
2024-10-03 22:47:03 -0400

    Add WEEK4_1 folder

---
+++ WEEK4_1/Procrastination Police/Procrastination_PoliceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Procrastination_PoliceApp.swift
+//  Procrastination Police
+//
+//  Created by Enock Mecheo on 28/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Procrastination_PoliceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
