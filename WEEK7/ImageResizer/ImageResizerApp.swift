de9fae354ecfd8ade246f7d9092650a944a25207
Enock Mecheo <emm10042@nyu.edu>
2024-10-24 23:02:34 -0400

    WEEK7 initial commit

---
+++ WEEK7/ImageResizer/ImageResizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageResizerApp.swift
+//  ImageResizer
+//
+//  Created by Enock Mecheo on 24/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ImageResizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
