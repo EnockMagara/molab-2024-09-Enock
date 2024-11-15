c64c8d6b112fd3232fe43e5ba116272251962379
Enock Mecheo <emm10042@nyu.edu>
2024-11-14 23:17:54 -0500

    initial commit

---
+++ WEEK10/communityMaps/communityMapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  communityMapsApp.swift
+//  communityMaps
+//
+//  Created by Enock Mecheo on 14/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct communityMapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
